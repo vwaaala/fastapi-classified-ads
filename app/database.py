ce2abd11e6d1d1552c8ff0cc84e51550f6982949
mr <vwaaala@gmail.com>
2021-11-01 05:02:23 +0600

    Added Fastapi Pydantic Sqlalchemy

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import config
+
+# engine = create_engine("mysql+pymysql://root:tanmay@localhost:3306/supercar", pool_size=100, max_overflow=0)
+engine = create_engine("mysql+pymysql://mr:X112233x@localhost:3306/nextgen", pool_size=100, max_overflow=0)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# Dependency
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        # noinspection PyUnboundLocalVariable
+        db.close()
+
---
+++ app/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="SuperCar Backend API",
+    version="0.0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"version": "v1.0.0"}
+
+
+if __name__ == '__main__':
+    # from app.database import Base, engine
+    # Base.metadata.create_all(bind=engine)
+    # uvicorn.run(app)
+    uvicorn.run("main:app", reload=True)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, workers=4)
